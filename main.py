c2f5caa581281e13795494e4b0bb40ff963a417f
romad <romadmitr12@gmail.com>
2022-01-27 19:20:30 +0700

    Заменил запрос на статичный. Создается таблица основная без данных. Создаются каталоги таблицы с уникальными значениями столбцов из animals.db

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sqlite3
 
 
-def database_select(sqlite_query, name_database, params):
+def database_select(sqlite_query, params):
     if params == 0:
-        con = sqlite3.connect(f"{name_database}.db")
+        con = sqlite3.connect("animal.db")
         cur = con.cursor()
         cur.execute(sqlite_query)
         data_return = cur.fetchall()
         con.close()
     else:
-        con = sqlite3.connect(f"{name_database}.db")
+        con = sqlite3.connect(f"animal.db")
         cur = con.cursor()
         cur.execute(sqlite_query, params)
         data_return = cur.fetchall()
@@ -17,8 +17,8 @@ def database_select(sqlite_query, name_database, params):
     return data_return
 
 
-def database_insert(sqlite_query, name_database, params):
-    con = sqlite3.connect(f"{name_database}.db")
+def database_insert(sqlite_query, params):
+    con = sqlite3.connect(f"animal.db")
     cur = con.cursor()
     if params != 0:
         for i in params:
@@ -31,100 +31,156 @@ def database_insert(sqlite_query, name_database, params):
 
 def create_database():
     query = """
-    CREATE TABLE IF NOT EXISTS age(
-    id_age_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    age_upon_outcome TEXT
-    );
-    CREATE TABLE IF NOT EXISTS type(
-    id_type_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    animal_type TEXT
-    );
-    CREATE TABLE IF NOT EXISTS name(
-    id_name_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT DEFAULT 'NoName'
-    );
-    CREATE TABLE IF NOT EXISTS appearance(
-    id_appearance_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    breed TEXT,
-    color1 TEXT,
-    color2 TEXT
-    );
-    CREATE TABLE IF NOT EXISTS date_of_birth(
-    id_date_of_birth_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    date_of_birth TEXT
-    );
-    CREATE TABLE IF NOT EXISTS program(
-    id_program_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    outcome_subtype TEXT DEFAULT 'NoProgram'
-    );
-    CREATE TABLE IF NOT EXISTS what_now(
-    id_what_now_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    outcome_type TEXT DEFAULT 'Unknown'
-    );
-    CREATE TABLE IF NOT EXISTS date_input(
-    id_date_input_table INTEGER PRIMARY KEY AUTOINCREMENT,
-    outcome_month TEXT,
-    outcome_year TEXT
-    );
-    CREATE TABLE IF NOT EXISTS animals_new(
-    id_animals INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS animal_color_all_color
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    color VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS animal_age
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    age VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS animal_type
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    type VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS animal_name
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    name VARCHAR(100) DEFAULT 'NoName'
+);
+
+CREATE TABLE IF NOT EXISTS animal_breed
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    breed VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS animal_color
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    id_color_1 INTEGER,
+    id_color_2 INTEGER,
+    FOREIGN KEY (id_color_1) REFERENCES animal_color_all_color(id),
+    FOREIGN KEY (id_color_2) REFERENCES animal_color_all_color(id)
+);
+
+CREATE TABLE IF NOT EXISTS animal_date_of_birth
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    date_of_birth VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS animal_program
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    program VARCHAR(50) DEFAULT 'NoProgram'
+);
+
+CREATE TABLE IF NOT EXISTS animal_where_now
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    where_now VARCHAR(50) DEFAULT 'Unknown'
+);
+
+CREATE TABLE IF NOT EXISTS animal_new_month_departure
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    month INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS animal_new_year_departure
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    year INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS animal_new
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
     id_age INTEGER,
     id_type INTEGER,
     id_name INTEGER,
-    id_appearance INTEGER,
+    id_breed INTEGER,
+    id_color INTEGER,
     id_date_of_birth INTEGER,
     id_program INTEGER,
-    id_what_now INTEGER,
-    id_date_input INTEGER,
-    FOREIGN KEY (id_age) REFERENCES age (id_age_table) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_type) REFERENCES type (id_type_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_name) REFERENCES name (id_name_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_appearance) REFERENCES appearance (id_appearance_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_date_of_birth) REFERENCES date_of_birth (id_date_of_birth_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_program) REFERENCES program (id_program_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_what_now) REFERENCES what_now (id_what_now_table)ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (id_date_input) REFERENCES date_input (id_date_input_table)ON DELETE SET NULL ON UPDATE CASCADE
-    );
-    """
-    database_insert(query, 'animals_new', 0)
+    id_where_now INTEGER,
+    id_month_departure INTEGER,
+    id_year_departure INTEGER,
+    FOREIGN KEY (id_year_departure) REFERENCES animal_new_year_departure(id),
+    FOREIGN KEY (id_month_departure) REFERENCES animal_new_month_departure(id),
+    FOREIGN KEY (id_where_now) REFERENCES animal_where_now(id),
+    FOREIGN KEY (id_program) REFERENCES animal_program(id),
+    FOREIGN KEY (id_date_of_birth) REFERENCES animal_date_of_birth(id),
+    FOREIGN KEY (id_color) REFERENCES animal_color(id),
+    FOREIGN KEY (id_breed) REFERENCES animal_breed(id),
+    FOREIGN KEY (id_name) REFERENCES animal_name(id),
+    FOREIGN KEY (id_type) REFERENCES animal_type(id),
+    FOREIGN KEY (id_age) REFERENCES animal_age(id)
+);
+"""
+    database_insert(query, 0)
 
 
 def drop_database():
     query = """
-    DROP TABLE animals_new;
-    DROP TABLE age;
-    DROP TABLE appearance;
-    DROP TABLE date_input;
-    DROP TABLE date_of_birth;
-    DROP TABLE name;
-    DROP TABLE program;
-    DROP TABLE type;
-    DROP TABLE what_now;
+    DROP TABLE animal_new;
+    DROP TABLE animal_age;
+    DROP TABLE animal_breed;
+    DROP TABLE animal_color;
+    DROP TABLE animal_color_all_color;
+    DROP TABLE animal_name;
+    DROP TABLE animal_new_month_departure;
+    DROP TABLE animal_new_year_departure;
+    DROP TABLE animal_where_now;
+    DROP TABLE animal_program;
+    DROP TABLE animal_type;
+    DROP TABLE animal_date_of_birth;
     """
-    database_insert(query, 'animals_new', 0)
+    database_insert(query, 0)
 
 
-def super_insert_database_func():
-    dict_create_query = {
-        'age': ('age_upon_outcome',),
-        'type': ('animal_type',),
-        'name': ('name',),
-        'appearance': ('breed', 'color1', 'color2'),
-        'date_of_birth': ('date_of_birth',),
-        'program': ('outcome_subtype',),
-        'what_now': ('outcome_type',),
-        'date_input': ('outcome_year', 'outcome_month')
-    }
-    new_list = []
-    for key, value in dict_create_query.items():
-        query = f"""SELECT DISTINCT {','.join(value)} FROM animals ORDER BY {','.join(value)}"""
-        list_sql = database_select(query, 'animal', 0)
-        query = f"""
-        INSERT INTO {key} ({','.join(value)})
-        values ({','.join(['?']*len(value))})"""
-        database_insert(query, 'animals_new', list_sql)
+def insert_no_main_tables():
+    query = """
+INSERT INTO animal_age (age)
+SELECT DISTINCT age_upon_outcome FROM animals;
+
+INSERT INTO animal_breed (breed)
+SELECT DISTINCT breed FROM animals;
+
+INSERT INTO animal_color (id_color_1, id_color_2)
+SELECT DISTINCT color1,color2 FROM animals;
+
+INSERT INTO animal_date_of_birth (date_of_birth)
+SELECT DISTINCT date_of_birth FROM animals;
+
+INSERT INTO animal_name (name)
+SELECT DISTINCT name FROM animals;
+
+INSERT INTO animal_new_month_departure (month)
+SELECT DISTINCT outcome_month FROM animals;
+
+INSERT INTO animal_new_year_departure (year)
+SELECT DISTINCT outcome_year FROM animals;
+
+INSERT INTO animal_program (program)
+SELECT DISTINCT outcome_subtype FROM animals;
+
+INSERT INTO animal_type (type)
+SELECT DISTINCT animal_type FROM animals;
+
+INSERT INTO animal_where_now (where_now)
+SELECT DISTINCT outcome_type FROM animals;
+    """
+    database_insert(query, 0)
 
 
 create_database()
 #drop_database()
-super_insert_database_func()
+insert_no_main_tables()

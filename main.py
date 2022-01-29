6f5fdb2b1738828779c35dbc92860ae400cef962
romad <romadmitr12@gmail.com>
2022-01-30 02:43:33 +0700

    Не сделано до конца Проблемы со ссылкой на два цвета.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sqlite3
+from flask import Flask
+
+
+app = Flask(__name__)
 
 
 def database_select(sqlite_query, params):
@@ -61,15 +65,6 @@ CREATE TABLE IF NOT EXISTS animal_breed
     breed VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS animal_color
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT ,
-    id_color_1 INTEGER,
-    id_color_2 INTEGER,
-    FOREIGN KEY (id_color_1) REFERENCES animal_color_all_color(id),
-    FOREIGN KEY (id_color_2) REFERENCES animal_color_all_color(id)
-);
-
 CREATE TABLE IF NOT EXISTS animal_date_of_birth
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT ,
@@ -107,7 +102,8 @@ CREATE TABLE IF NOT EXISTS animal_new
     id_type INTEGER,
     id_name INTEGER,
     id_breed INTEGER,
-    id_color INTEGER,
+    id_color_1 INTEGER,
+    id_color_2 INTEGER,
     id_date_of_birth INTEGER,
     id_program INTEGER,
     id_where_now INTEGER,
@@ -118,7 +114,8 @@ CREATE TABLE IF NOT EXISTS animal_new
     FOREIGN KEY (id_where_now) REFERENCES animal_where_now(id),
     FOREIGN KEY (id_program) REFERENCES animal_program(id),
     FOREIGN KEY (id_date_of_birth) REFERENCES animal_date_of_birth(id),
-    FOREIGN KEY (id_color) REFERENCES animal_color(id),
+    FOREIGN KEY (id_color_1) REFERENCES animal_color_all_color(id),
+    FOREIGN KEY (id_color_2) REFERENCES animal_color_all_color(id),
     FOREIGN KEY (id_breed) REFERENCES animal_breed(id),
     FOREIGN KEY (id_name) REFERENCES animal_name(id),
     FOREIGN KEY (id_type) REFERENCES animal_type(id),
@@ -140,10 +137,6 @@ INSERT INTO animal_color_all_color (color)
 SELECT DISTINCT trim(color1) as color_result FROM animals
 UNION
 SELECT DISTINCT trim(color2) as color_result FROM animals;
-
-INSERT INTO animal_color (id_color_1, id_color_2)
-SELECT DISTINCT trim(color1), trim(color2)
-FROM animals;
 
 INSERT INTO animal_date_of_birth (date_of_birth)
 SELECT DISTINCT date_of_birth FROM animals;
@@ -171,16 +164,25 @@ SELECT DISTINCT outcome_type FROM animals;
 
 def insert_main_table():
     query = """
-    SELECT age_upon_outcome, animal_type, name, breed, color1, color2,
-    date_of_birth, outcome_subtype, outcome_type, outcome_month, outcome_year 
-    FROM animals LIMIT 1"""
-    database_select(query, 0)
-
-
-def update_table():
+    INSERT INTO animal_new (id_age, id_type, id_name, id_breed, id_date_of_birth, 
+    id_program, id_where_now, id_month_departure, id_year_departure, id_color_1, id_color_2)
+    SELECT age_upon_outcome, animal_type, name, breed, date_of_birth, 
+    outcome_subtype, outcome_type, outcome_month, outcome_year, color1, color2 
+    FROM animals;
+    """
+    database_insert(query, 0)
     query = """
-    UPDATE animal_color
-    SET id_color_1 = (SELECT id FROM animal_color_all_color WHERE id_color_1 = color),
+    UPDATE animal_new
+    SET id_age = (SELECT id FROM animal_age WHERE id_age = age),
+        id_type = (SELECT id FROM animal_type WHERE id_type = type),
+        id_name = (SELECT id FROM animal_name WHERE id_name = name),
+        id_breed = (SELECT id FROM animal_breed WHERE id_breed= breed),
+        id_date_of_birth = (SELECT id FROM animal_date_of_birth WHERE id_date_of_birth = date_of_birth),
+        id_program = (SELECT id FROM animal_program WHERE id_program = program),
+        id_where_now = (SELECT id FROM animal_where_now WHERE id_where_now = where_now),
+        id_month_departure = (SELECT id FROM animal_new_month_departure WHERE id_month_departure = month),
+        id_year_departure = (SELECT id FROM animal_new_year_departure WHERE id_year_departure = year),
+        id_color_1 = (SELECT id FROM animal_color_all_color WHERE id_color_1 = color),
         id_color_2 = (SELECT id FROM animal_color_all_color WHERE id_color_2 = color);"""
     database_insert(query, 0)
 
@@ -190,7 +192,6 @@ def drop_database():
     DROP TABLE animal_new;
     DROP TABLE animal_age;
     DROP TABLE animal_breed;
-    DROP TABLE animal_color;
     DROP TABLE animal_color_all_color;
     DROP TABLE animal_name;
     DROP TABLE animal_new_month_departure;
@@ -203,8 +204,36 @@ def drop_database():
     database_insert(query, 0)
 
 
-create_database()
-#drop_database()
-insert_no_main_tables()
-insert_main_table()
-update_table()
+def save_func():
+    create_database()
+    insert_no_main_tables()
+    insert_main_table()
+    #drop_database()
+
+
+@app.route('/id/<uid>')
+def return_data_from_database(uid):
+    uid = int(uid)
+    query = f"""
+    SELECT age, breed, date_of_birth, name, month, year, program, type, where_now, color FROM animal_new
+    LEFT JOIN animal_age aa on aa.id = animal_new.id_age
+    LEFT JOIN animal_breed ab on ab.id = animal_new.id_breed
+    LEFT JOIN animal_date_of_birth adob on adob.id = animal_new.id_date_of_birth
+    LEFT JOIN animal_name an on animal_new.id_name = an.id
+    LEFT JOIN animal_new_month_departure anmd on animal_new.id_month_departure = anmd.id
+    LEFT JOIN animal_new_year_departure anyd on animal_new.id_year_departure = anyd.id
+    LEFT JOIN animal_program ap on animal_new.id_program = ap.id
+    LEFT JOIN animal_type at on animal_new.id_type = at.id
+    LEFT JOIN animal_where_now awn on animal_new.id_where_now = awn.id
+    LEFT JOIN animal_color_all_color on animal_new.id_color_1 = animal_color_all_color.id
+    WHERE animal_new.id = {uid}
+    ;
+    """
+    data_return = database_select(query, 0)
+    return str(data_return)
+
+
+#save_func()
+
+if __name__ == '__main__':
+    app.run()
